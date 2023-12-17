b2b014dfcddb732c905a62f2d8a960f6362b0a1b
laimamus <mia.kastehein@gmail.com>
2023-12-17 22:22:03 +0200

    commit

---
+++ 12. tund/router_server/server.js
@@ -0,0 +1,28 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+function serverFile(path, contentType, res) {
+    fs.readFile(path, (err, data) => {
+        if (err) {
+            res.writeHead(500, {'Content-Type': 'text/plain'});
+            return res.end('Error faili laadimisega');
+        }
+        res.writeHead(200, {'Content-Type': contentType});
+        res.end(data);
+    })
+}
+
+const server = http.createServer((req, res) => {
+    if (req.url === '/') {
+        serveFile('index.html', 'text/html', res);
+    } else if (req.url === '/about') {
+        serveFile('about.html', 'tetx/html', res);
+    } else if (req.url.match('\.css$')) {
+        var cssPath = path.join(_dirname, req.url);
+        serveFile(cssPath, 'text/css', res);
+    } else {
+        res.writeHead(404, {'Content-Type': 'text/plain'});
+        res.end('Lehte ei leitud');
+        }
+})
